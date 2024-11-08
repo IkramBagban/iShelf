abbc49740b38cbd1afc94324389b5d43b620ae92
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2024-11-08 20:07:35 +0530

    design: create basic structure and simple design

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='w-full flex justify-around items-center my-3 border-b'>
+        <div>Logo</div>
+        <div>Menu</div>
+    </div>
+  )
+}
+
+export default Navbar
