3af01d15e468aebf7203037d89c8b702c26c29f1
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2024-11-08 20:18:24 +0530

    design: design Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Button } from "@/components/ui/button";
+
 
 const Navbar = () => {
   return (
-    <div className='w-full flex justify-around items-center my-3 border-b'>
-        <div>Logo</div>
-        <div>Menu</div>
+    <div className="w-full flex justify-between items-center p-4 border-b bg-white shadow-sm">
+      <div className="flex items-center space-x-4">
+        <div className="text-lg font-bold">iShelf</div>
+      </div>
+      {/* <div className="flex space-x-4">
+        <Button variant="outline">Home</Button>
+        <Button variant="outline">Categories</Button>
+        <Button variant="outline">Add Resource</Button>
+      </div> */}
     </div>
-  )
-}
+  );
+};
+
 
 export default Navbar
